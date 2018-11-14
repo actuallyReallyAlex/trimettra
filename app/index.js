34549d84a44205fa5d05d2cab5b80334be97f7cc
Alex Lee <alex@alex-lee.site>
2018-11-14 08:43:01 -0800

    📦 Really basic func to print a message to screen through Messaging API

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+// Import the messaging module
+import * as messaging from "messaging";
+// Import document
+import document from "document";
+
+let log = document.getElementById("log");
+log.text = "hullo";
+
+// Listen for the onmessage event
+messaging.peerSocket.onmessage = function(evt) {
+  // Output the message to the console
+  const readable = JSON.stringify(evt.data);
+  console.log(readable);
+  log.text = readable;
+};
